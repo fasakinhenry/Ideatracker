75b3f3c58dde04068fdbeb8df89e34f197a85e77
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-31 03:05:18 +0100

    feat: update the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Login } from "./pages/Login";
 import { Home } from "./pages/Home";
-import { UserProvider, useUser } from "./lib/context/user";
+import { UserProvider } from "./lib/context/user";
+import { IdeasProvider } from "./lib/context/ideas";
 
 function App() {
   const isLoginPage = window.location.pathname === "/login";
@@ -8,32 +9,12 @@ function App() {
   return (
     <div>
       <UserProvider>
-        <Navbar /> {/* Add the navbar before page content */}
-        <main>{isLoginPage ? <Login /> : <Home />}</main>
-      </UserProvider>
+        <IdeasProvider>
+          <Navbar /> {/* Add the navbar before page content */}
+          <main>{isLoginPage ? <Login /> : <Home />}</main>
+        </IdeasProvider>
+      <UserProvider>
     </div>
-  );
-}
-
-function Navbar() {
-  const user = useUser();
-
-  return (
-    <nav>
-      <a href="/">Idea tracker</a>
-      <div>
-        {user.current ? (
-          <>
-            <span>{user.current.email}</span>
-            <button type="button" onClick={() => user.logout()}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <a href="/login">Login</a>
-        )}
-      </div>
-    </nav>
   );
 }
 
