1e90f6440e23540d6a60d570a0505e13ec3ffbfe
Fasakin Henry <fasakinhenry@gmail.com>
2024-10-31 02:17:49 +0100

    feat: add appwrite js file library

---
+++ src/lib/appwrite.js
@@ -0,0 +1,9 @@
+import { Client } from 'appwrite';
+
+const client = new Client();
+client
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject('6722d96d000cfdc2503e');
+
+export const account = new Account(client);
+export const databases = new Databases(client);
